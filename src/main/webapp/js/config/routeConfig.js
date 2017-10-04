bcba06ed3d5216c61b5f34e80347731891631ec0
Adbys Vasconcelos <adbysvasconcelos@Air-de-Adbys.home>
2017-10-04 16:34:31 -0300

    adicionado routes

--- src/main/webapp/js/config/routeConfig.js
+++ src/main/webapp/js/config/routeConfig.js
@@ -1,3 +1,24 @@
 app.config(function ($routeProvider){
+	$routeProvider
+    .when("/index", {
+        templateUrl : "views/cadastraPedido.html",
+        controller: "indexCtrl"
+    })
+    .when("/buscar", {
+        templateUrl : "views/buscar.html",
+        controller: "indexCtrl"
+    })
+    .when("/marca", {
+        templateUrl : "views/marca.html",
+        controller: "marcaCtrl"
+    })
+    .when("/loja", {
+        templateUrl : "views/loja.html",
+        controller: "lojaCtrl"
+    })
+    .when("/categoria", {
+        templateUrl : "views/categoria.html",
+        controller: "categoriaCtrl"
+    })
 
 });
