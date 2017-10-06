96081c86a9abd5a95c7a8eec9fd531f4af92d6e4
Adbys Vasconcelos <adbysvasconcelos@Air-de-Adbys.home>
2017-10-06 12:56:45 -0300

    modificando table

--- src/main/webapp/js/controller/pedidosCtrl.js
+++ src/main/webapp/js/controller/pedidosCtrl.js
@@ -1,8 +1,43 @@
 app.controller("pedidosCtrl", function($scope, $http){
 
 	$scope.pedidos = [];
+	$scope.marcas = [];
+	$scope.lojas = [];
 
 	carregarPedidos();
+	carregarLojas();
+	carregarMarcas();
+
+	function carregarLojas () {
+
+		console.log("Carregando Lojas")
+		$http.get("/loja").then(function successCallback(response) {
+		    console.log(response);
+		    for (index in response.data) {
+		    	$scope.lojas.push(response.data[index]);
+		    }
+		    console.log($scope.lojas)
+		  }, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+
+	}
+
+	function carregarMarcas () {
+
+		console.log("Carregando Marcas")
+		$http.get("/marca").then(function successCallback(response) {
+		    console.log(response);
+		    for (index in response.data) {
+		    	$scope.marcas.push(response.data[index]);
+		    }
+		  }, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		  });
+
+	}
 
 
 
